d738622aa12e016c5fe8aa3f14f06d8ffcdd72dc
Gepardi-dot <ari.gepardi@gmail.com>
2025-09-05 15:03:56 +0200

    Add auth page to display Supabase Auth componentCreate page.tsx

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,11 @@
+import AuthComponent from '../auth'
+
+export default function AuthPage() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-12">
+      <div className="container mx-auto px-4">
+        <AuthComponent />
+      </div>
+    </div>
+  )
+}
